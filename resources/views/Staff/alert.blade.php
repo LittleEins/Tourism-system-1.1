852469a50606426187144aac78472eee4588d1a0
LittleEinstein ID 73795 | HSP#0439 <jezteribasan@gmail.com>
2022-11-29 01:51:41 +0800

    Revert "Revert "Spelling Fixes for Sidebars""
    This reverts commit 07f7957da1c434d51d562df8decf59d1730fd890.

--- resources/views/Staff/alert.blade.php
+++ resources/views/Staff/alert.blade.php
@@ -15,7 +15,7 @@
 
   <main id="main" class="main">
     <div class="pagetitle">
-      <h1>Alert</h1>
+      <h1>Manage Notification</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Notification</a></li>
