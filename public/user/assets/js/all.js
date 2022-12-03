1cda2b0200f7ffa7bd2077200f40b155788d8908
JerhomeVlogs <jerhomereantaso85@gmail.com>
2022-12-03 14:41:52 +0800

    update dashboard admin live count interval

--- public/user/assets/js/all.js
+++ public/user/assets/js/all.js
@@ -16,6 +16,7 @@ $(document).ready(function ()
             {
                 let l = response.count;
                 $('#dahboard').html("");
+                $('#dahboard2').html("");
                 for (let i = 0; i < l; i++)
                 {
                     $('#dahboard').append('<div class="col-sm-6 col-xl-3">\
@@ -28,21 +29,22 @@ $(document).ready(function ()
                     </div>\
                   </div>')
 
-                //   $('#dahboard').append('<div class="col-sm-6 col-xl-3">\
-                //   <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">\
-                //     <i class="fa fa-users fa-3x text-primary"></i>\
-                //     <div id="falls_count" class="ms-3">\
-                //         <p class="mb-2">'+response.data[i].name+'</p>\
-                //         <h6 id="patar_count" class="mb-0">'+response.data[i].visit_count+'</h6>\
-                //     </div>\
-                //   </div>\
-                // </div>')
+                  $('#dahboard2').append('<div class="col-sm-6 col-xl-3">\
+                  <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">\
+                    <i class="fa fa-users fa-3x text-primary"></i>\
+                    <div id="falls_count" class="ms-3">\
+                        <p class="mb-2">'+response.data[i].name+'</p>\
+                        <h6 id="patar_count" class="mb-0">'+response.data[i].total_visit+'</h6>\
+                    </div>\
+                  </div>\
+                </div>')
                 }
 
             }
  
         });
     }
+    
 
     function book_count ()
     {
