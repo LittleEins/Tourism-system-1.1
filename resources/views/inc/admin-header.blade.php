4c974ca34f6fd1a98a956812397edd48ee6e9d00
JerhomeVlogs <jerhomereantaso85@gmail.com>
2022-12-09 11:10:56 +0800

    remove profile

--- resources/views/inc/admin-header.blade.php
+++ resources/views/inc/admin-header.blade.php
@@ -71,21 +71,6 @@
             <li>
               <hr class="dropdown-divider">
             </li>
-
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="{{route('profilestaff.view')}}">
-                <i class="bi bi-person"></i>
-                <span>My Profile</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-
-
            
             <li>
               <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}">
